78bfb9c1c4afc64cb7725dc2e54a2887aad45d67
Ketan <ket@spiritquest.co.uk>
2013-02-12 00:14:48 +0000

    callback topology edit

--- app.js
+++ app.js
@@ -1,52 +1,71 @@
 // Ti.API.info('Starting ACS start up process - getting device token')
- if (Ti.Properties.getBool('acs-init')) {
-	Ti.API.warn('********* ACS login already initialised ******** ');
-	return;
- }
- Ti.Properties.setBool('acs-init', true);
- var acs_ph = require('/modules/ACS_Push_Helper');
- var ACS = new acs_ph.ACSpush();
- /**
-  * First checking to see if the user is logged into Appcelerator Cloud Services.
-  */
- ACS.showLoggedInACSuser();
- /**
-  * Performs checks to see if the device has a token and if not creates one, storing the value 
-  * into persistent storage.
-  */
- ACS.getDeviceToken();
- ACS.deviceToken = Ti.App.Properties.getString('deviceToken');
+// if (Ti.App.Properties.getBool('acs-init')) {
+// Ti.API.warn('********* ACS login already initialised ******** ');
+// break;
+// }
 
+Ti.App.Properties.setBool('acs-init', true);
+var acs_ph = require('/modules/ACS_Push_Helper');
+var ACS = new acs_ph.ACSpush();
 
- /**
-  * Timeout Loop block - checks to see if the logged in state is true, after a small delay for network check,
-  * this ideally needs to be asynchronous, but the code will need refactoring for that
-  */
+var deviceCallback = function() {
+    Ti.API.warn(' ********* Device Callback ******** ');
+    /**
+     * Performs checks to see if the device has a token and if not creates one, storing the value
+     * into persistent storage.
+     */
 
+    var loginCallback = function() {
+        Ti.API.warn(' ********* Login Callback ******** ');
+        ACS.deviceToken = Ti.App.Properties.getString('deviceToken');
+        /**
+         * Checks to see if the logged in state is true, after a small delay for network check,
+         * this ideally needs to be asynchronous, but the code will need refactoring for that
+         */
 
- setTimeout(function() {
-     if (!ACS.loggedInToACS) {
-         Ti.API.warn('********* This device is NOT Logged into ACS ******** ');
-         Ti.API.warn('********* About to Query ACS userbase against this device ******** ');
-         ACS.queryNewACSuser({
-			username : ACS.deviceToken,
-			callback : function(){
-				alert('Create a new user? ' + ACS.createNewUser)
-			}
-         });
+        if (!ACS.loggedInToACS) {
+            Ti.API.warn(' ********* This device is NOT Logged into ACS ******** ');
+            Ti.API.warn('********* About to Query ACS userbase against this device ******** ');
 
-         setTimeout(function() {
-             if(ACS.createNewUser){
-                 Ti.API.warn('********* Need to create a new ACS user account: '+ ACS.createNewUser+' ******** ');
-                 ACS.createUserAccount();
-             }
+            Ti.API.warn(' ********* Setting up callbacks ******** ');
 
-             setTimeout(function() {
-                 Ti.API.warn('Login to ACS - ' + ACS.createNewUser);
-                 ACS.loginUserToACS();
-             }, 4000);
-         }, 5400);
-     } else {
-         Ti.API.warn('********* USer Logged IN true - no need to create new account ******** ');
-     }
- }, 4000);
+            var loginUser = function() {
+                Ti.API.warn(' ********* Login Callback ******** ');
+                Ti.API.warn(' ********* Login user to ACS - ' + ACS.createNewUser + ' ******** ');
+                ACS.loginUserToACS();
+            };
+            var queryCallback = function() {
+                Ti.API.warn(' ********* Query Callback ******** ');
+                if (ACS.createNewUser) {
+                    Ti.API.warn(' ********* Need to create a new ACS user account: ' + ACS.createNewUser + ' ******** ');
+                    ACS.createUserAccount({
+                        callback : loginUser
+                    });
+                } else {
+                    loginUser();
+                }
+            };
+
+            ACS.queryNewACSuser({
+                username : ACS.deviceToken,
+                callback : queryCallback
+            });
+
+        } else {
+            Ti.API.warn(' ********* USer Logged IN true - no need to create new account ******** ');
+        }
+    };
+
+    ACS.getDeviceToken({
+        callback : loginCallback
+    });
+
+};
+
+/**
+ * First checking to see if the user is logged into Appcelerator Cloud Services.
+ */
+ACS.showLoggedInACSuser({
+    callback : deviceCallback
+});
+
